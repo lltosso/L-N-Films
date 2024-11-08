a65dcb5dabf38b84c190161c3b22a1c84ced1ed1
lltosso <luis.tosso@gmail.com>
2024-11-08 14:23:34 +0100

    añadido nuevos

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "../Componentes/molecules/Header"
+
+
+const Layout = () => {
+  return (
+    <>
+    <Header/>
+    <main>Este es mi contenido dinamico</main>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
